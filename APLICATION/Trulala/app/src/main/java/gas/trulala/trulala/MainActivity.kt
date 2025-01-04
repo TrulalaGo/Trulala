0bfffc69d0f732460d7e5b2de07c46bbab7e387e
TrulalaGo <trobeltrulala87@gmail.com>
2025-01-04 19:31:35 +0700

    Update MainActivity.kt

--- APLICATION/Trulala/app/src/main/java/gas/trulala/trulala/MainActivity.kt
+++ APLICATION/Trulala/app/src/main/java/gas/trulala/trulala/MainActivity.kt
@@ -1,6 +1,21 @@
+
 package gas.trulala.trulala
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.widget.*
+import java.io.File
+import android.view.View
 
-class MainActivity{
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val menubtn = findViewById<Button>(R.id.menubtn)
+        val liner = findViewById<LinearLayout>(R.id.liner)
+        menubtn.setOnClickListener{
+        liner.visibility = if(liner.visibility == View.GONE){View.VISIBLE}
+        else{View.GONE}
+        }
+   }
 }
