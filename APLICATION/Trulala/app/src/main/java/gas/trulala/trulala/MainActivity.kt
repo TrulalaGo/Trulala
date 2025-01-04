f67b11021edf309f4fd528ac6b787a9a0e18a2de
TrulalaGo <trobeltrulala87@gmail.com>
2025-01-04 23:55:36 +0700

    Update MainActivity.kt

--- APLICATION/Trulala/app/src/main/java/gas/trulala/trulala/MainActivity.kt
+++ APLICATION/Trulala/app/src/main/java/gas/trulala/trulala/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.*
 import java.io.File
 import android.view.View
+import android.content.Intent
+import android.widget.Toast
 
 public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,32 @@ public class MainActivity : AppCompatActivity() {
         liner.visibility = if(liner.visibility == View.GONE){View.VISIBLE}
         else{View.GONE}
         }
+        val simpan = findViewById<Button>(R.id.simpan)
+        val mulai = findViewById<Button>(R.id.mulai)
+        mulai.setOnClickListener{
+        val intent = Intent(this, Mulai::class.java)
+        startActivity(intent)
+        }
+        simpan.setOnClickListener{
+        Toast.makeText(this, "Menyimpan", Toast.LENGTH_SHORT).show()
+        }
+        val editin = findViewById<EditText>(R.id.editin)
+        val koreksi = findViewById<TextView>(R.id.koreksi)
+        val gd = findViewById<Button>(R.id.gradle)
+        val bo = findViewById<Button>(R.id.buildozer)
+        val py = findViewById<Button>(R.id.python)
+        val kt = findViewById<Button>(R.id.kotlin)
+        val kodej = findViewById<Button>(R.id.java)
+        val js = findViewById<Button>(R.id.javascript)
+        val html = findViewById<Button>(R.id.html)
+        val cc = findViewById<Button>(R.id.cc)
+        gd.setOnClickListener{}
+        bo.setOnClickListener{}
+        py.setOnClickListener{}
+        kt.setOnClickListener{}
+        kodej.setOnClickListener{}
+        js.setOnClickListener{}
+        html.setOnClickListener{}
+        cc.setOnClickListener{}
    }
 }
